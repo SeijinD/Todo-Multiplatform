770c8c55ae79d987515e067774445c19a7f93ea9
George Karanikolas <gkaranikolas@arx.net>
2026-05-29 21:37:10 +0300

    [Core] Add Todo domain model

---
+++ core/src/commonMain/kotlin/com/seijind/todo/model/Todo.kt
@@ -0,0 +1,10 @@
+package com.seijind.todo.model
+
+data class Todo(
+    val id: String,
+    val title: String,
+    val notes: String,
+    val isCompleted: Boolean,
+    val createdAt: Long,
+    val updatedAt: Long,
+)
